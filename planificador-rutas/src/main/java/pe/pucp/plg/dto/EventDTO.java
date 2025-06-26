61c500576408ae314f322d887884f6fe14ef0979
Piero Alonso Alvarez Castillo <167045705+Piero-Alvarez-C@users.noreply.github.com>
2025-06-25 23:55:59 -0500

    Update EventDTO.java

--- planificador-rutas/src/main/java/pe/pucp/plg/dto/EventDTO.java
+++ planificador-rutas/src/main/java/pe/pucp/plg/dto/EventDTO.java
@@ -1,19 +1,15 @@
 package pe.pucp.plg.dto;
-
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import pe.pucp.plg.dto.enums.EventType;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 public class EventDTO {
     private EventType type;
     private Object payload;
     private long timestamp;
 
-    public EventDTO() {}
 
     public EventDTO(EventType type, Object payload, long timestamp) {
         this.type = type;
